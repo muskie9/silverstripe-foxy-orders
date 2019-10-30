1216018bd20948749d0c808c0184fac1d52725c3
Jason Irish <jsirish@users.noreply.github.com>
2019-10-30 17:18:06 -0400

    BUGFIX Travis phpcs config (#52)

--- src/Factory/OrderFactory.php
+++ src/Factory/OrderFactory.php
@@ -51,8 +51,10 @@ class OrderFactory extends FoxyFactory
         $transaction = $this->getTransaction()->getParsedTransactionData()->getField('transaction');
 
         /** @var $order Order */
-        if ($transaction->hasField('id')
-            && !($order = Order::get()->filter('OrderID', $transaction->getField('id'))->first())) {
+        if (
+            $transaction->hasField('id')
+            && !($order = Order::get()->filter('OrderID', $transaction->getField('id'))->first())
+        ) {
             $order = Order::create();
         }
 
